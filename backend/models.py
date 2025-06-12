8ec42ec23bfad5c7658055cca5dffc50ea92ca60
EnejiStats <enejistats@gmail.com>
2025-06-12 04:19:18 +0100

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(20))  # player, scout, club
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    nationality = db.Column(db.String(50))
+    position = db.Column(db.String(30))
+
+class MatchStat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey("player.id"))
+    goals = db.Column(db.Integer)
+    assists = db.Column(db.Integer)
+    shots = db.Column(db.Integer)
+    yellow_cards = db.Column(db.Integer)
+    red_cards = db.Column(db.Integer)
+    minutes_played = db.Column(db.Integer)
+    match_date = db.Column(db.String(20))
