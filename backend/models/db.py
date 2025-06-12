a30c4ec6ba7a7395da4d705e002f057cd8c20bf6
EnejiStats <enejistats@gmail.com>
2025-06-12 09:49:13 +0100

    Create db.py

---
+++ backend/models/db.py
@@ -0,0 +1,12 @@
+
+from pymongo import MongoClient
+import os
+
+client = MongoClient(os.getenv('MONGODB_URI', 'mongodb://localhost:27017'))
+db = client['enejistats']
+
+users = db['users']
+players = db['players']
+clubs = db['clubs']
+matches = db['matches']
+stats = db['stats']
